b5e87255550d464a05d9d3b03cb327cde44eb8b0
Katarzyna Izak <kasik83@gmail.com>
2018-11-19 23:21:52 +0100

    refactoring -> class

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-const imagesPaths = ['images/DSCN3277.JPG','images/DSCN3312.JPG','images/DSCN3316.JPG','images/DSCN3360.JPG']
+const imagesPaths = ['images/DSCN3277.JPG',
+                    'images/DSCN3312.JPG',
+                    'images/DSCN3316.JPG',
+                    'images/DSCN3360.JPG']
 
 const imagesGallery = document.getElementById('images')
 
-imagesGallery.addEventListener('click',({target}) => {
-    target.classList.toggle('image--favourite')
-})
+class Image {
+    constructor(path) {
+        this.path = path
+    }
+
+    show() {
+        const image = document.createElement('img')
+        image.setAttribute('src', this.path)
+        image.addEventListener('click', () => this.markOrUnmarkFav())
+        imagesGallery.appendChild(image)
+        this.imageElement = image
+    }
+
+    markOrUnmarkFav() {
+        this.imageElement.classList.toggle('image-favourite')
+    }
+}
 
 imagesPaths.forEach((image) => {
-    const newImage = document.createElement('img')
-    newImage.setAttribute('src', image)
-    imagesGallery.appendChild(newImage)
+    const newImage = new Image(image)
+    newImage.show()
 })
