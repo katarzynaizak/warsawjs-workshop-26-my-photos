2f2f23ee10e9b3aefc73f5c56f5c66a4cb738036
Katarzyna Izak <kasik83@gmail.com>
2018-11-19 22:29:58 +0100

    wyswietla zdjecia z katalogu za pomoca tablicy i petli

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const imagesPaths = ['images/DSCN3277.JPG','images/DSCN3312.JPG','images/DSCN3316.JPG','images/DSCN3360.JPG']
+
+const imagesGallery = document.getElementById('images')
+
+imagesPaths.forEach((image) => {
+    const newImage = document.createElement('img')
+    newImage.setAttribute('src', image)
+    imagesGallery.appendChild(newImage)
+})
